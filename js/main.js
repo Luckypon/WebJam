c8d3d0974c24be6eb5304a20800e62174aeb557b
maureenroche <roche.maureen@gmail.com>
2017-12-02 20:15:14 -0500

    navigation with ajax

--- js/main.js
+++ js/main.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
 
-	function reset(){
-		$(".question").hide();
-		$(".question:nth-child(1)").show();
-		$(".calque").hide();
-	};
+	reset();
 
-
-	$('.question > button').click(function(e){
-		var parent = $(this).parent();
-		var data1 = parent.data("value");
-		var data2 = $(this).data("value");
-
-		var element = $("#"+data1);
-		var img = $("#"+data1+" > img");
-		var imageURL = "img/"+data1+data2+".jpg";
-
-		img.attr("src", imageURL);
-
-		element.show();
-		parent.hide();
-		parent.next('.question').show();
-
+	// EVENTS
+	$("#intro").on( "click", 'button', function(){
+		requestAjax("question1", "#questions");
+		$("#start").remove();
 	});
 
-	$('#reset').click(function(e){
+	$('#questions').on( "click.", 'button', showImage);
+
+	$('#questions').on( "click", '#reset', function(){
 		reset();
 	});
 
@@ -36,7 +22,57 @@ $(document).ready(function() {
 		$("#infos").hide();
 	});
 
-	reset();
-
 });
 
+/******** Change HTLM content with AJAX ********/
+//argument file = the file within the content you want to add
+function requestAjax(file, container) {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+      ajaxData(xhr.responseText, container);
+    }
+  };
+  xhr.open("GET", file+".html", true);
+  xhr.send(null);
+}
+
+function ajaxData(data, container) {
+	$(container).html(data);
+}
+
+/******** Reset the canvas ********/
+function reset(){
+	requestAjax("start", "#intro");
+	$("#questions").empty();
+	$(".calque").hide();
+}
+
+/******** Reset the canvas ********/
+
+function showImage() {
+	var parent = $(this).parent();
+	var data1 = parent.data("value");
+	var data2 = $(this).data("value");
+
+	var element = $("#"+data1);
+	var img = $("#"+data1+" > img");
+	var imageURL = "img/"+data1+data2+".jpg";
+
+	img.attr("src", imageURL);
+	element.show();
+
+	var number = parent.data("number");
+	nextQuestion(number);
+}
+
+function nextQuestion(number){
+	if (number == 2) {
+		requestAjax("reset", "#questions");
+	}
+	else{
+		requestAjax("question"+(number+1), "#questions");
+	}
+}
